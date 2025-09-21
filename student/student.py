eddc875f9d8b92e5e9633e4d0b589a7d1284893b
asma2003 <asma200m2200@gmail.com>
2025-09-21 11:47:58 +0300

    add grade to student and add getting average method

--- student/student.py
+++ student/student.py
@@ -2,10 +2,31 @@ class Student:
     #store a student's name and list of grades 
     def __init__(self, name):
         self.name = name 
-        self.grade = []
+        self.grades = []
+    def add_grade(self, grade):
+        # method to add a grade (0 to 100 only). invalid grades should raise error.
+        if grade >= 0 and grade <= 100:
+            self.grades.append(grade)
+        else:
+            raise ValueError('Grade must be between 0 and 100 inclusive')    
+    def average(self):
+        if not self.grades:
+            return 0
+
+        total = 0
+        for grade in self.grades:
+            total += grade
+        average = total / len(self.grades)
+        return average        
+
+
 
 if  __name__ == '__main__':      
     student_name = input('Enter the new student name: ')
-    
     new_student = Student(student_name)
     print(f'Welcome {new_student.name}')
+    new_grade = input('Add grade to student: ')
+    try:
+        new_student.add_grade(int(new_grade))
+    except ValueError as err:
+        print(err)    
--- test/test_student.py
+++ test/test_student.py
@@ -2,7 +2,37 @@ import unittest
 from student.student import Student 
 
 class TestStudent(unittest.TestCase):
+    
+    def setUp(self):
+        self.student = Student('Asma')
 
     def test_create_student_object(self):
-        new_student = Student('Asma')
-        self.assertEqual(new_student.name, 'Asma')
+        # new_student = Student('Asma')
+        self.assertEqual(self.student.name, 'Asma')
+
+    def test_add_valid_grade(self):
+        self.student.add_grade(50)
+        self.assertIn(50, self.student.grades)  
+        self.student.add_grade(100)
+        self.assertIn(100, self.student.grades)
+        self.student.add_grade(0)
+        self.assertIn(0, self.student.grades)            
+
+    def test_add_invalid_grade(self):
+          with self.assertRaises(ValueError):
+             self.student.add_grade(101)
+             
+          with self.assertRaises(TypeError):  
+              self.student.add_grade('hello')
+
+          with self.assertRaises(ValueError):    
+            self.student.add_grade(-1)
+    
+    
+    def test_average_no_grades(self):
+        self.assertEqual(self.student.average(), 0)
+
+    def test_average_with_grades(self):
+        for grade in [20, 40, 60]:
+            self.student.add_grade(grade)
+        self.assertEqual(self.student.average(), 40)    
